8c3785d35b1cf0ed11a8da612be84008960a3756
mchjess <15751@coderacademy.edu.au>
2024-07-02 10:52:40 +1000

    Code for task management part of application added.

--- productivity_tracker.py
+++ productivity_tracker.py
@@ -1,7 +1,11 @@
 import time
+import csv
+
+
+#Timer 
 
 class Task:
-    #Starts new task and includes name and category for reporting.
+    #Creates new task and includes name and category for reporting.
     def __init__(self, name, category) :
         self.name = name
         self.category = category
@@ -22,5 +26,33 @@ class Task:
     # Returns the total time spent on a task.
     def get_time_spent(self):
         if self.start is not None:
-            return self.total_time + time.time() - self.start_time
-            return self.total_time 
+            return self.total_time + time.time() - self.start_time 
+        return self.total_time 
+
+#Task manager
+
+class ProductivityTracker:
+    def __init__(self):
+        self.tasks = []
+    
+    #Adding a task
+    def add_task(self, name, category):
+        self.tasks.append(Task(name,category))
+    
+    #Starting the task
+    def start_task(self, name):
+        for task in self.tasks:
+            if task.name == name:
+                task.start()
+                print(f"Started task: {name}")
+                return
+            print(f"Task {name} not found.")
+    
+    #Stopping the task
+    def stop_task(self,name):
+        for task in self.tasks:
+            if task.name == name:
+                task.stop()
+                print(f"Task {name} stopped.")
+                return
+            print(f"Task {name} not found.")
